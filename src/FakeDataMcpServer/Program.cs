87dfe49bac6521cead88c4a82da99504e3d14d7f
Renato Groffe <renato.groffe@yahoo.com.br>
2026-01-28 01:25:06 -0300

    Commit inicial

---
+++ src/FakeDataMcpServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using FakeDataMcpServer.Tools;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.AddConsole(o => o.LogToStandardErrorThreshold = LogLevel.Trace);
+
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools<ContatosFakeDataTool>()
+    .WithTools<EmpresasFakeDataTool>()
+    .WithTools<ProdutosFakeDataTool>()
+    .WithTools<MensagensFakeDataTool>();
+
+await builder.Build().RunAsync();
